27e39e526431f0332230401af7217e19e4e43073
ayushmankumar7 <ayushmankumar7@gmail.com>
2020-03-26 10:21:40 +0530

    firemod Module

---
+++ firemod.py
@@ -0,0 +1,31 @@
+import google.cloud
+import serial
+import firebase_admin
+import time
+import datetime
+import ast
+from firebase_admin import firestore
+from firebase_admin import credentials
+
+def send_it(data, firebase_admin = firebase_admin, credentials = credentials, firestore = firestore):
+	if (not len(firebase_admin._apps)):
+		
+		cred = credentials.Certificate("measure-distance-ac87a-firebase-adminsdk-7fb2c-36a9368d56.json")
+
+		default_app = firebase_admin.initialize_app(cred,
+			{'projectID': 'measure-distance-ac87a'
+			})
+
+	db = firestore.client()
+
+	ref = db.collection(u'person')
+
+	try:
+		docs = ref.get()
+		for doc in docs:
+			print(u'Doc Data:{}'.format(doc.to_dict()))
+	except google.cloud.exceptions.NotFound:
+		print(u'Missing data')
+		
+	
+	ref.document("people").set({u'object_list':data})
